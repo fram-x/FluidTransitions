bf276f8c236eaeb97d33b92c685d2a0991076510
Christian Falch <christian.falch@gmail.com>
2018-03-09 20:28:20 +0100

    Added imports for extra views.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import FluidNavigator from './src/FluidNavigator';
 import Transition from './src/TransitionView';
+import TransitionView from './src/TransitionItemsView';
+import TransitionRouteView from './src/TransitionRouteView';
 
-export { FluidNavigator, Transition };
+export { FluidNavigator, Transition, TransitionView, TransitionRouteView };
