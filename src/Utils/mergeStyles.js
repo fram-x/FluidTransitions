425044ae4f2ba38e8cb66ec217cf51e7611f388b
Christian Falch <christian.falch@gmail.com>
2018-04-09 15:42:48 +0200

    Added test for numeric styles.

--- src/Utils/mergeStyles.js
+++ src/Utils/mergeStyles.js
@@ -2,20 +2,26 @@ import { StyleSheet } from 'react-native';
 
 const mergeStyles = (styles: Array<any>): StyleSheet.NamedStyles => {
   const retVal = { transform: [] };
-  styles.forEach(s => {
-    if(s){
-    Object.keys(s).forEach(key => {
-      if(s[key] && s[key] instanceof Array){
-        const a = s[key];
+  styles.forEach(style => {
+    if(style){
+      let flattenedStyle = style;
+      if(isNumber(flattenedStyle))
+        flattenedStyle = StyleSheet.flatten(flattenedStyle);
+
+    Object.keys(flattenedStyle).forEach(key => {
+      if(flattenedStyle[key] && flattenedStyle[key] instanceof Array){
+        const a = flattenedStyle[key];
         if(!retVal[key]) retVal[key] = [];
         a.forEach(i => retVal[key].push(i));
-      } else if(s[key]) {
-        retVal[key] = s[key];
+      } else if(flattenedStyle[key]) {
+        retVal[key] = flattenedStyle[key];
       }
     })
   }})
 
   return retVal;
 }
+
+function isNumber(n) { return !isNaN(parseFloat(n)) && !isNaN(n - 0); }
 
 export { mergeStyles };
