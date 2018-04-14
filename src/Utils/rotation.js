c27b5a827a21cada4e113af2abf234d70da37f73
Christian Falch <christian.falch@gmail.com>
2018-04-14 22:45:25 +0200

    Added rotation module

---
+++ src/Utils/rotation.js
@@ -0,0 +1,79 @@
+
+import { Metrics } from './../Types/Metrics';
+
+export type GetOriginalRectParameters = {
+  boundingBox: Metrics, // Bounding box
+  theta: number, // Original rotation in radians
+}
+export const getOriginalRect = (params: GetOriginalRectParameters): Metrics => {
+  if (params.theta === 0) {
+    return params.boundingBox;
+  }
+
+  const { x, y, width, height } = params.boundingBox;
+  const cos = Math.cos(-params.theta);
+  const sin = Math.sin(-params.theta);
+
+  // Get rotated height/width
+  let a = (cos ** 2) - (sin ** 2);
+  if (Math.abs((cos ** 2) - (sin ** 2)) <= Number.EPSILON) { a = 0.5; }
+  const nw = a * (width * cos - height * sin);
+  const nh = a * (-width * sin + height * cos);
+
+  return {
+    x: Math.round(x + (width - Math.abs(nw)) * 0.5),
+    y: Math.round(y + (height - Math.abs(nh)) * 0.5),
+    width: Math.abs(Math.round(nw)),
+    height: Math.abs(Math.round(nh)),
+  };
+};
+
+export type RotatePointParameters = {
+  x: number,
+  y: number,
+  cx: number,
+  cy: number,
+  theta: number,
+}
+export const rotatePoint = (params: RotatePointParameters) => {
+  const { x, y, cx, cy, theta } = params;
+  const cos = Math.cos(theta);
+  const sin = Math.sin(theta);
+
+  const nx = (cos * (x - cx)) + (sin * (y - cy)) + cx;
+  const ny = (cos * (y - cy)) - (sin * (x - cx)) + cy;
+
+  return { x: nx, y: ny };
+};
+
+export type GetBoundingBoxParameters = {
+  rect: Metrics,
+  theta: number,
+};
+export const getBoundingBox = (params: GetBoundingBoxParameters) => {
+  const { x, y, width, height } = params.rect;
+  const { theta } = params;
+
+  const cx = x + (width / 2);
+  const cy = y + (height / 2);
+
+  const tl = rotatePoint({ x, y, cx, cy, theta });
+  const bl = rotatePoint({ x, y: y + height, cx, cy, theta });
+  const tr = rotatePoint({ x: x + width, y, cx, cy, theta });
+  const br = rotatePoint({ x: x + width, y: y + height, cx, cy, theta });
+
+  const minX = Math.min(tl.x, bl.x, tr.x, br.x);
+  const maxX = Math.max(tl.x, bl.x, tr.x, br.x);
+  const minY = Math.min(tl.y, bl.y, tr.y, br.y);
+  const maxY = Math.max(tl.y, bl.y, tr.y, br.y);
+
+  return {
+    y: minY,
+    height: maxY - minY,
+    x: minX,
+    width: maxX - minX,
+  };
+};
+
+export const degToRad = (deg: number): number => deg * Math.PI / 180;
+export const radToDeg = (rad: number): number => rad * 180 / Math.PI;
---
+++ src/__tests__/rotation.spec.js
@@ -0,0 +1,120 @@
+import { getOriginalRect, rotatePoint, getBoundingBox, degToRad } from './../Utils/rotation';
+import { Metrics } from './../Types/Metrics';
+
+describe('getRotatedRect', () => {
+  it('returns original rect when rotation is 0', () => {
+    const boundingBox = { x: 100, y: 100, width: 100, height: 100 };
+    const valueToTest = getOriginalRect({ boundingBox, theta: 0 });
+    expect(valueToTest).toBe(boundingBox);
+  });
+
+  it('returns original rect when rotation is -20', () => {
+    const boundingBox: Metrics = {
+      x: 85.91436179442114,
+      y: 149.91436179442115,
+      width: 128.1712764111577,
+      height: 128.1712764111577,
+    };
+    const valueToTest = getOriginalRect({ boundingBox, theta: degToRad(-20) });
+    expect(valueToTest).toEqual({ x: 100, y: 164, width: 100, height: 100 });
+  });
+
+  it('returns original rect when rotation is -20', () => {
+    const theta = degToRad(-20);
+    const rect = { x: 300, y: 64, width: 40, height: 50 };
+    const boundingBox = getBoundingBox({ rect, theta });
+    const valueToTest = getOriginalRect({ boundingBox, theta });
+    expect(valueToTest).toEqual(rect);
+  });
+
+  it('returns original rect when rotation is 0', () => {
+    const theta = degToRad(0);
+    const rect = { x: 100, y: 164, width: 100, height: 100 };
+    const boundingBox = getBoundingBox({ rect, theta });
+    const valueToTest = getOriginalRect({ boundingBox, theta });
+    expect(valueToTest).toEqual(rect);
+  });
+
+  it('returns original rect when rotation is 45', () => {
+    const theta = degToRad(45);
+    const rect = { x: 10, y: 10, width: 40, height: 40 };
+    const boundingBox = getBoundingBox({ rect, theta });
+    const valueToTest = getOriginalRect({ boundingBox, theta });
+    expect(valueToTest).toEqual(rect);
+  });
+
+  it('returns original rect when rotation is 90', () => {
+    const theta = degToRad(90);
+    const rect = { x: 100, y: 164, width: 100, height: 100 };
+    const boundingBox = getBoundingBox({ rect, theta });
+    const valueToTest = getOriginalRect({ boundingBox, theta });
+    expect(valueToTest).toEqual(rect);
+  });
+
+  it('returns original rect when rotation is 135', () => {
+    const theta = degToRad(135);
+    const rect = { x: 100, y: 164, width: 100, height: 100 };
+    const boundingBox = getBoundingBox({ rect, theta });
+    const valueToTest = getOriginalRect({ boundingBox, theta });
+    expect(valueToTest).toEqual(rect);
+  });
+
+  it('returns original rect when rotation is 180', () => {
+    const theta = degToRad(180);
+    const rect = { x: 100, y: 164, width: 100, height: 100 };
+    const boundingBox = getBoundingBox({ rect, theta });
+    const valueToTest = getOriginalRect({ boundingBox, theta });
+    expect(valueToTest).toEqual(rect);
+  });
+
+  it('returns original rect when rotation is 225', () => {
+    const theta = degToRad(225);
+    const rect = { x: 100, y: 164, width: 100, height: 100 };
+    const boundingBox = getBoundingBox({ rect, theta });
+    const valueToTest = getOriginalRect({ boundingBox, theta });
+    expect(valueToTest).toEqual(rect);
+  });
+
+  it('returns original rect when rotation is 270', () => {
+    const theta = degToRad(270);
+    const rect = { x: 100, y: 164, width: 100, height: 100 };
+    const boundingBox = getBoundingBox({ rect, theta });
+    const valueToTest = getOriginalRect({ boundingBox, theta });
+    expect(valueToTest).toEqual(rect);
+  });
+});
+
+describe('rotatePoint', () => {
+  it('returns rotated point for angle of 20 degrees', () => {
+    const rect = { x: 100, y: 164, width: 100, height: 100 };
+    const cx = rect.x + (rect.width / 2);
+    const cy = rect.y + (rect.height / 2);
+    const rotatedPoint = rotatePoint({ x: rect.x, y: rect.y, cx, cy, theta: degToRad(20) });
+    expect(rotatedPoint).toEqual({ x: 85.91436179442114, y: 184.116376126988 });
+  });
+});
+
+describe('getBoundingBox', () => {
+  it('returns correct bounding box for rect and rotation -20', () => {
+    const rect = { x: 100, y: 164, width: 100, height: 100 };
+    const bb = getBoundingBox({ rect, theta: degToRad(-20) });
+    expect(bb).toEqual({
+      x: 85.91436179442114,
+      y: 149.91436179442115,
+      width: 128.1712764111577,
+      height: 128.1712764111577,
+    });
+  });
+
+  it('returns correct bounding box for rect and rotation 45', () => {
+    const rect = { x: 100, y: 164, width: 100, height: 100 };
+    const bb = getBoundingBox({ rect, theta: degToRad(45) });
+    expect(bb).toEqual({
+      height: 141.4213562373095,
+      width: 141.4213562373095,
+      x: 79.28932188134524,
+      y: 143.28932188134524,
+    });
+  });
+});
+
