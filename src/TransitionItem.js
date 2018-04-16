95db2e8aac6fa72c254cd3239bb4bef22e5226cd
Christian Falch <christian.falch@gmail.com>
2018-04-16 16:20:47 +0200

    Fixed issue with negative angles.

--- src/TransitionItem.js
+++ src/TransitionItem.js
@@ -11,8 +11,8 @@ type Size = {
 export default class TransitionItem {
   constructor(
     name: string, route: string, reactElement: Object,
-    shared: boolean, appear: string, disappear: string, 
-    delay: boolean
+    shared: boolean, appear: string, disappear: string,
+    delay: boolean,
   ) {
     this.name = name;
     this.route = route;
@@ -32,7 +32,7 @@ export default class TransitionItem {
   disappear: string | Function
   delay: boolean
   layoutReady: boolean
-  flattenedStyle: ?any  
+  flattenedStyle: ?any
 
   getNodeHandle() {
     return this.reactElement.getNodeHandle();
@@ -43,45 +43,49 @@ export default class TransitionItem {
   }
 
   getFlattenedStyle() {
-    if(!this.flattenedStyle) {
+    if (!this.flattenedStyle) {
       const child = React.Children.only(this.reactElement.props.children);
-      const style = child.props.style;
-      if(!style) return null;
+      const { style } = child.props;
+      if (!style) return null;
       this.flattenedStyle = StyleSheet.flatten(style);
     }
     return this.flattenedStyle;
   }
 
-  updateMetrics(viewMetrics: Metrics, itemMetrics: Metrics) {    
+  updateMetrics(viewMetrics: Metrics, itemMetrics: Metrics) {
     const { x, y, width, height } = itemMetrics;
 
     const ri = this.getRotation();
     const t = this.getRotationRad(ri);
-    
-    if(t !== 0) {      
-      const r = getOriginalRect({ boundingBox: { x, y, width, height }, theta: t });
-      this.metrics = {x: r.x - viewMetrics.x, y: r.y - viewMetrics.y, width: r.width, height: r.height };        
+
+    if (t !== 0) {
+      const r = getOriginalRect({
+        boundingBox: { x, y, width, height },
+        theta: t,
+        skipWidth: Platform.OS === 'android',
+      });
+      this.metrics = { x: r.x - viewMetrics.x, y: r.y - viewMetrics.y, width: r.width, height: r.height };
     } else {
-      this.metrics = {x: x - viewMetrics.x, y: y - viewMetrics.y, width, height };        
+      this.metrics = { x: x - viewMetrics.x, y: y - viewMetrics.y, width, height };
     }
 
-    // console.log({ 
-    //   r: this.name + "/" + this.route,
-    //   org: { x: itemMetrics.x, y: itemMetrics.y, width: itemMetrics.width, height: itemMetrics.height},
-    //   new: { x: this.metrics.x, y: this.metrics.y, width: this.metrics.width, height: this.metrics.height}
-    // });
-  }  
+    console.log({
+      r: `${this.name}/${this.route}`,
+      org: { x: itemMetrics.x, y: itemMetrics.y, width: itemMetrics.width, height: itemMetrics.height },
+      new: { x: this.metrics.x, y: this.metrics.y, width: this.metrics.width, height: this.metrics.height },
+    });
+  }
 
   getRotation() {
     const ri = getRotationFromStyle(this.getFlattenedStyle());
-    let retVal = { type: 'unknown', value: 0};
-    if(ri.rotate) {      
-      if(ri.rotate.rotate){
+    let retVal = { type: 'unknown', value: 0 };
+    if (ri.rotate) {
+      if (ri.rotate.rotate) {
         const rotation: String = ri.rotate.rotate;
-        if(rotation.endsWith('deg')){
-          retVal = { type: 'deg', value: Math.abs(parseInt(rotation.substring(0, rotation.length-3)))};
-        } else if(rotation.endsWith('rad')) {
-          retVal = { type: 'rad', value: parseInt(rotation.substring(0, rotation.length-3))};
+        if (rotation.endsWith('deg')) {
+          retVal = { type: 'deg', value: parseInt(rotation.substring(0, rotation.length - 3)) };
+        } else if (rotation.endsWith('rad')) {
+          retVal = { type: 'rad', value: parseInt(rotation.substring(0, rotation.length - 3)) };
         }
       }
     }
@@ -89,17 +93,17 @@ export default class TransitionItem {
   }
 
   getRotationRad(ri) {
-    if(ri.type === 'deg') return this.getDegreesToRadians(ri.value);
+    if (ri.type === 'deg') return this.getDegreesToRadians(ri.value);
     return ri.value;
   }
 
   getRotationDeg(ri) {
-    if(ri.type === 'rad') return this.getRadiansToDegrees(ri.value);
+    if (ri.type === 'rad') return this.getRadiansToDegrees(ri.value);
     return ri.value;
   }
 
   getDegreesToRadians = (degrees: number): number => degrees * Math.PI / 180;
-  getRadiansToDegrees = (rad: number): number => rad * 180/Math.PI;
+  getRadiansToDegrees = (rad: number): number => rad * 180 / Math.PI;
 
   scaleRelativeTo(other: TransitionItem): Size {
     const validate = i => {
