d6ad58a1b81893e88699e3f133a96a5de2748dee
Christian Falch <christian.falch@gmail.com>
2018-05-22 21:32:21 +0200

    Fixed issue with packages in Android project

--- Examples/android/app/src/main/java/com/examples/MainApplication.java
+++ Examples/android/app/src/main/java/com/examples/MainApplication.java
@@ -25,8 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new VectorIconsPackage(),
+          new MainReactPackage(),            
             new SvgPackage(),
             new VectorIconsPackage()
       );
