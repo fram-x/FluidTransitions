0fd8b6b503fd7b496be3004f92b82e517c7c4895
Christian Falch <christian.falch@gmail.com>
2018-05-22 21:20:25 +0200

    Added react-native-svg package and link

--- Examples/android/app/src/main/java/com/examples/MainApplication.java
+++ Examples/android/app/src/main/java/com/examples/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.horcrux.svg.SvgPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new SvgPackage(),
             new VectorIconsPackage()
       );
     }
