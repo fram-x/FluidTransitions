2c2e1b2c11306bb84bb23c5058e19da49fb7b730
Christian Falch <christian.falch@gmail.com>
2018-06-15 14:49:11 +0200

    Index.js now exports both createFluidNavigator and FluidNavigator

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import FluidNavigator from './src/FluidNavigator';
+import createFluidNavigator from './src/createFluidNavigator';
 import Transition from './src/TransitionView';
 
 export { RouteDirection, Metrics, TransitionSpecification } from './src/Types';
-export { FluidNavigator, Transition };
+export { FluidNavigator, createFluidNavigator, Transition };
