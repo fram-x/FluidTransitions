301fa48b5cb5f5f96d4953c0f20e6acf264db010
Christian Falch <christian.falch@gmail.com>
2018-09-27 20:56:57 +0200

    Fixed styles in getResolvedMetrics to use stylesheet styles.

--- Examples/SharedElements.js
+++ Examples/SharedElements.js
@@ -105,8 +105,8 @@ class Screen1 extends React.Component {
   }
 
   componentWillMount() {
-    const items = getRandomImages(5, Dimensions.get('window').width);
-    const users = getRandomImages(5, 30).map(img => ({
+    const items = getRandomImages(15, Dimensions.get('window').width);
+    const users = getRandomImages(15, 30).map(img => ({
       source: img,
       name: 'User name',
     }));
